cca3993dcd6c32bdaad4c30b4f9a4da470a8c5b2
shingteha <Tahiti2021>
2022-01-13 03:17:00 +1300

    Links final fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
