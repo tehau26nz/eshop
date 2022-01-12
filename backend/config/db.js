ee15d95feafd9e6e5aa84afb2abab0267dbf3495
shingteha <Tahiti2021>
2022-01-13 11:38:29 +1300

    colors added

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {})
-    console.log(`MongoDB connected: ${conn.connection.host}`)
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.error(`Error: ${error.message}}`)
+    console.error(`Error: ${error.message}}`.red.underline.bold)
     process.exit(1)
   }
 }
