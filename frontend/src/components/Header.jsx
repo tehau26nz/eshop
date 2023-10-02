cd79af4d03c07aa31f5f7e426343c92c879b4bb2
Tehaunui Shing Soi <92765882+tehau26nz@users.noreply.github.com>
2023-10-03 11:04:16 +1300

    Added style and products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser} from 'react-icons/fa';
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">Tshop</Navbar.Brand>
+                <Navbar.Brand href="/">
+                  <img src={logo} alt="Tshop logo"/>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav"/>
                     <Nav className="ms-auto">
