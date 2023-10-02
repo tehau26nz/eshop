a6e94bcd24af11f66d402d594c756c9d59c5abcd
Tehaunui Shing Soi <92765882+tehau26nz@users.noreply.github.com>
2023-10-02 18:01:35 +1300

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <Routes>
-            <Route path='/' element={<HomeScreen />} exact />
-            <Route path='/product/:id' element={<ProductScreen />} />
-          </Routes>
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+    <h1>Welcome to Tshop</h1>
   )
 }
 
