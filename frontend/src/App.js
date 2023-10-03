c72a8c86c64818516cbf9c1f98ccfc6f59a195b9
Tehaunui Shing Soi <92765882+tehau26nz@users.noreply.github.com>
2023-10-03 14:58:18 +1300

    Fixed code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <Header/>
       <main className="py-3">
         <Container>
-          <Outlet/>>
+          <Outlet/>
         </Container>
       </main>
       <Footer/>
