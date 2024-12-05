5a8867fd7fb155fb29db945ce6b4e1c001ab806b
Tehaunui <tehaunuiss26@gmail.com>
2024-12-05 16:42:20 +1300

    Additional temp fix for products

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
 import {
   Button,
   Card,
@@ -11,19 +11,21 @@ import {
 } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import Rating from "../components/Rating";
+import products from "../products";
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState({});
+  // const [product, setProduct] = useState({});
   const { id: productId } = useParams();
+  const product = products.find((p) => p._id === productId);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    };
+  // useEffect(() => {
+  //   const fetchProduct = async () => {
+  //     const { data } = await axios.get(`/api/products/${productId}`);
+  //     setProduct(data);
+  //   };
 
-    fetchProduct();
-  }, [productId]);
+  //   fetchProduct();
+  // }, [productId]);
 
   return (
     <>
